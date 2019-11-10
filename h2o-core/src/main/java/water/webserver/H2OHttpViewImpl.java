de9f4925b192dc4c03527c1be3490abdf665ae05
Jan Sterba <jan.sterba@h2o.ai>
2019-11-10 18:11:27 +0100

    PUBDEV-7056: fix SPNEGO auth

--- h2o-core/src/main/java/water/webserver/H2OHttpViewImpl.java
+++ h2o-core/src/main/java/water/webserver/H2OHttpViewImpl.java
@@ -68,16 +68,18 @@ public class H2OHttpViewImpl implements H2OHttpView {
       //TODO for LoginType.HASH, this equals not adding the handler at all; consinder this optimization
       return false;
     }
-
-    if (request.getUserPrincipal() == null) {
-      return false;
+    
+    if (request.getUserPrincipal() != null) {
+      String loginName = request.getUserPrincipal().getName();
+      if (loginName.equals(config.user_name)) {
+        return false;
+      } else {
+        Log.warn("Login name (" + loginName + ") does not match cluster owner name (" + config.user_name + ")");
+        ServletUtils.sendResponseError(response, HttpServletResponse.SC_UNAUTHORIZED, "Login name does not match cluster owner name");
+      }
+    } else {
+      ServletUtils.sendResponseError(response, HttpServletResponse.SC_UNAUTHORIZED, "Missing authentication details");
     }
-    final String loginName = request.getUserPrincipal().getName();
-    if (loginName.equals(config.user_name)) {
-      return false;
-    }
-    Log.warn("Login name (" + loginName + ") does not match cluster owner name (" + config.user_name + ")");
-    ServletUtils.sendResponseError(response, HttpServletResponse.SC_UNAUTHORIZED, "Login name does not match cluster owner name");
     return true;
   }
 
